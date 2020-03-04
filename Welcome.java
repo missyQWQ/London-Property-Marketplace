ea94e1cf77f070e952dc1c916378a4123385dee4
Zejin Deng <k1894658@kcl.ac.uk>
2020-03-04 14:13:16 +0000

    I didn't change anything. i was trying to add the ActionListener in the welcome class so that we can define the client or the officer login. However i met some problem.

--- Welcome.java
+++ Welcome.java
@@ -24,29 +24,33 @@ public class Welcome extends JFrame
         westP.setBounds(0,0,300,300);
         westP.add(l);
         c.add(westP);
-                
-        JLabel l1 = new JLabel("Username"), l2 = new JLabel("Password"),
-        l3 = new JLabel("Welcome to London Airbnb app!");
+
+        JLabel  l1 = new JLabel("Username"), 
+                l2 = new JLabel("Password"),
+                l3 = new JLabel("Welcome to London Airbnb app!");
         l1.setFont(new Font("Arial",Font.BOLD,15));
         l2.setFont(new Font("Arial",Font.BOLD,15));
         l3.setFont(new Font("Lucida Calligraphy",Font.BOLD,15));
         l3.setForeground(new Color(0,0,128));
         JButton btn = new JButton("Hello Airbnb!");
         btn.setBackground(Color.PINK);
-        btn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setRange();
-                dispose();
+        btn.addActionListener(new ActionListener() 
+            {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    setRange();
+                    dispose();
+                }
             }
-        });
-        
+
+        );
+
         JTextField jt = new JTextField();
         jt.setColumns(20);
-        
+
         JPasswordField jp = new JPasswordField();
         jp.setEchoChar('\u2605');
-        
+
         JPanel eastP = new JPanel();
         eastP.setLayout(new GridLayout(5,1,10,10));
         eastP.setBounds(300,60,170,180);
@@ -56,30 +60,30 @@ public class Welcome extends JFrame
         eastP.add(jp);
         eastP.add(btn);
         c.add(eastP);
-        
+
         JPanel southP = new JPanel();
         southP.setLayout(new FlowLayout());
         southP.setBounds(100,300,300,100);
         southP.add(l3);
         c.add(southP);
-        
+
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
-        
+
         JMenu menu = new JMenu("Help");
         menuBar.add(menu);
-        
+
         JMenuItem item = new JMenuItem("???");
         //item.addActionListener(e -> setRange());
-        
+
         menu.add(item);
 
         setVisible(true);
-        
+
     }
-    
+
     private void setRange(){
         SetRange.newDialog();
     }
-    
+
 }
