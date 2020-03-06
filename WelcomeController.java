e47eafd0b4d96f788596fc888e70f262d9a169d1
Liangjie Wang <liangjie.wang@kcl.ac.uk>
2020-03-06 13:26:43 +0000

    Give warning when user didn't enter both price range and min nights Once satisfied, click confirm and change to the map panel.

--- WelcomeController.java
+++ WelcomeController.java
@@ -2,14 +2,19 @@ import javafx.fxml.FXML;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
+
 // import javafx.application.Application;
-// import javafx.event.ActionEvent;
-// import javafx.event.EventHandler;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
+ import javafx.event.ActionEvent;
+ import javafx.event.EventHandler;
+ import javafx.scene.Node;
+ import javafx.scene.Scene;
+ import javafx.scene.Parent;
+ import javafx.stage.Stage;
+ import javafx.stage.Modality;
 // import javafx.scene.layout.*;
 // import java.net.URL;
-// import javafx.fxml.FXMLLoader;
+ import javafx.fxml.FXMLLoader;
+import java.io.IOException;
 
 /**
  * Write a description of JavaFX class Welcome here.
@@ -20,13 +25,41 @@ import javafx.scene.control.*;
 public class WelcomeController
 {
     private ObservableList<String> priceChoices = 
-        FXCollections.observableArrayList("apples", "oranges");
+        FXCollections.observableArrayList("below 50", "50-100","100-150","150-200","over 200");
     
     @FXML
     private ChoiceBox priceRange;
     
     @FXML
+    private TextField minNights;
+    
+    @FXML
+    private Button confirm;
+    @FXML
     private void initialize(){
         priceRange.setItems(priceChoices);
+    }
+    
+    @FXML
+    public void confirmAction(ActionEvent event) throws IOException{
+        if(priceRange.getValue() == null){
+            createWarningBox();
+        }else{
+            if(minNights.getText().isEmpty()){
+                Parent mapPanel = FXMLLoader.load(getClass().getResource("map.fxml"));
+                Stage mapStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                mapStage.setScene(new Scene(mapPanel));
+            }
+        }
+    }
+    
+    private void createWarningBox() throws IOException{
+        Parent warning = FXMLLoader.load(getClass().getResource("warning.fxml"));
+        Stage warningBox = new Stage();
+        warningBox.setTitle("Error!");
+        warningBox.initModality(Modality.APPLICATION_MODAL);
+        warningBox.setScene(new Scene(warning));
+        warningBox.setResizable(false);
+        warningBox.show();
     }
 }
