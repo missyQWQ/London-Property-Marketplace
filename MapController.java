a1706e9bf963f36d17020fced64eed8dd2830b43
Yichun Zhang <yichun.zhang@kcl.ac.uk>
2020-03-14 18:37:44 +0000

    Achieved getting number of properties of each borough filtered by price range.

--- MapController.java
+++ MapController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.shape.Circle;
 import java.util.ArrayList;
+import java.util.Arrays;
 /**
  * Write a description of JavaFX class MapController here.
  *
@@ -24,7 +25,27 @@ public class MapController extends Controller
     {
         selectedPriceRange.setText(getMinPrice() + " - " + getMaxPrice());
         
-        ArrayList<AirbnbListing> data_filterByPrice = AirbnbDataFilter.priceRange_filter(new AirbnbDataLoader().load(), getMinPrice(), getMaxPrice());
+        ArrayList<BoroughInfo> boroInfo = new ArrayList<>();
+        boroInfo.addAll(Arrays.asList(new BoroughInfo(eali), new BoroughInfo(bren), new BoroughInfo(redb), new BoroughInfo(walt), new BoroughInfo(hrgy),
+               new BoroughInfo(houn), new BoroughInfo(enfi), new BoroughInfo(barn), new BoroughInfo(hrrw), new BoroughInfo(hill), new BoroughInfo(rich),
+               new BoroughInfo(king), new BoroughInfo(sutt), new BoroughInfo(croy), new BoroughInfo(brom), new BoroughInfo(bexl), new BoroughInfo(have),
+               new BoroughInfo(bark), new BoroughInfo(lews), new BoroughInfo(gwch), new BoroughInfo(mert), new BoroughInfo(wand), new BoroughInfo(hamm), 
+               new BoroughInfo(kens), new BoroughInfo(wstm), new BoroughInfo(camd), new BoroughInfo(isli), new BoroughInfo(city), new BoroughInfo(hack),
+               new BoroughInfo(towh), new BoroughInfo(newh), new BoroughInfo(sthw), new BoroughInfo(lamb)));
+        
+        ArrayList<AirbnbListing> data_filterByPrice = priceRange_filter(getMinPrice(), getMaxPrice());
+        
+        for(AirbnbListing data : data_filterByPrice) {
+            for(BoroughInfo boro : boroInfo) {
+                if(data.getNeighbourhood().equals(boro.getFullName()))
+                    boro.incPropertyNum();
+            }
+        }
+        
+        for(BoroughInfo boro2 : boroInfo) {
+                System.out.println(boro2.getFullName() + " : " + boro2.getPropertyNum());
+            }
+        
     }
     
     /**
@@ -45,43 +66,14 @@ public class MapController extends Controller
         changePanel(e, "./fxml/statistics.fxml", "Statistics");
     }
     
-    private String getFullName(String labelName)
+    private ArrayList<AirbnbListing> priceRange_filter(int minPrice, int maxPrice)
     {
-        switch(labelName) {
-            case "eali": return "Ealing";
-            case "bren": return "Brent";
-            case "redb": return "Redbridge";
-            case "walt": return "Waltham Forest";
-            case "hrgy": return "Haringey";
-            case "houn": return "Hounslow";
-            case "enfi": return "Enfield";
-            case "barn": return "Barnet";
-            case "hrrw": return "Harrow";
-            case "hill": return "Hillingdon";
-            case "rich": return "Richmond upon Thames";
-            case "king": return "Kingston upon Thames";
-            case "sutt": return "Sutton";
-            case "croy": return "Croydon";
-            case "brom": return "Bromley";
-            case "bexl": return "Bexley";
-            case "have": return "Havering";
-            case "bark": return "Barking and Dagenham";
-            case "lews": return "Lewisham";
-            case "gwch": return "Greenwich";
-            case "mert": return "Merton";
-            case "wand": return "Wandsworth";
-            case "hamm": return "Hammersmith and Fulham";
-            case "kens": return "Kensington and Chelsea";
-            case "wstm": return "Westminster";
-            case "camd": return "Camden";
-            case "isli": return "Islington";
-            case "city": return "City of London";
-            case "hack": return "Hackney";
-            case "towh": return "Tower Hamlets";
-            case "newh": return "Newham";
-            case "sthw": return "Southwark";
-            case "lamb": return "Lambeth";
-            default: return null;
+        ArrayList<AirbnbListing> totalData = new AirbnbDataLoader().load();
+        ArrayList<AirbnbListing> data = new ArrayList<>();
+        for(AirbnbListing list : totalData) {
+            if(list.getPrice() >= minPrice && list.getPrice() <= maxPrice)
+                data.add(list);
         }
+        return data;
     }
 }
