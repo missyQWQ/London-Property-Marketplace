dab615bf04616a149fa82bfb02fc3d3fd6b992b4
Runlin Zhou <k1924780@kcl.ac.uk>
2020-03-15 18:03:43 +0000

    modified

--- StatisticsController.java
+++ StatisticsController.java
@@ -44,7 +44,7 @@ public class StatisticsController extends Controller
     @FXML private Label info3;
     // Label for the fourth information box.
     @FXML private Label info4;
-    
+
     // An array of 8 title to show the properties
     private String[] title = new String[8];
     // An array of 8 information to show the properties
@@ -53,22 +53,22 @@ public class StatisticsController extends Controller
     private boolean[] isDisplayed = new boolean[8];
     // An array to show 4 information which are shown on the panel now
     private int[] boxIndex = new int[4];
-    
+
     @FXML
     private void initialize()
     {
         for (int i = 0; i < 8; i++){
             isDisplayed[i] = false;
         }
-        title[0] = "0";
-        title[1] = "1";
-        title[2] = "2";
-        title[3] = "3";
-        title[4] = "4";
-        title[5] = "5";
-        title[6] = "6";
-        title[7] = "7";
-        
+        title[0] = "00";
+        title[1] = "11";
+        title[2] = "22";
+        title[3] = "33";
+        title[4] = "44";
+        title[5] = "55";
+        title[6] = "66";
+        title[7] = "77";
+
         information[0] = "0";
         information[1] = "1";
         information[2] = "2";
@@ -77,23 +77,22 @@ public class StatisticsController extends Controller
         information[5] = "5";
         information[6] = "6";
         information[7] = "7";
-        
-        
+
         title1.setText(title[0]);
         info1.setText(information[0]);
         boxIndex[0] = 0;
         isDisplayed[0] = true;
-        
+
         title2.setText(title[1]);
         info2.setText(information[1]);
         isDisplayed[1] = true;
         boxIndex[1] = 1;
-        
+
         title3.setText(title[2]);
         info3.setText(information[2]);
         isDisplayed[2] = true;
         boxIndex[2] = 2;
-        
+
         title4.setText(title[3]);
         info4.setText(information[3]);
         isDisplayed[3] = true;
@@ -109,18 +108,24 @@ public class StatisticsController extends Controller
         String source = event.getSource().toString();
         source = source.substring(source.indexOf('=') + 1, source.indexOf(','));
         switch (source){
-            case "n1" : info1.setText(information[getNextStat(0, isDisplayed)]);
+            case "n1" : info1.setText(information[getNextStat(0, isDisplayed)]); 
+                        title1.setText(title[getNextStat(0, isDisplayed)]);
             break;
+            
             case "n2" : info2.setText(information[getNextStat(1, isDisplayed)]);
+                        title2.setText(title[getNextStat(1, isDisplayed)]);
             break;
+            
             case "n3" : info3.setText(information[getNextStat(2, isDisplayed)]);
+                        title3.setText(title[getNextStat(2, isDisplayed)]);
             break;
+            
             case "n4" : info4.setText(information[getNextStat(3, isDisplayed)]);
+                        title4.setText(title[getNextStat(3, isDisplayed)]);
             break;
         }
     }
-    
-    
+
     /**
      * Action for the previous button on every of the information box.
      * @param event The action event to trigger the action.
@@ -131,16 +136,23 @@ public class StatisticsController extends Controller
         source = source.substring(source.indexOf('=') + 1, source.indexOf(','));
         switch (source){
             case "p1" : info1.setText(information[getPrevStat(0, isDisplayed)]);
-                break;
+                        title1.setText(title[getPrevStat(0, isDisplayed)]);
+            break;
+            
             case "p2" : info2.setText(information[getPrevStat(1, isDisplayed)]);
-                break;
+                        title2.setText(title[getPrevStat(1, isDisplayed)]);
+            break;
+            
             case "p3" : info3.setText(information[getPrevStat(2, isDisplayed)]);
-                break;
+                        title3.setText(title[getPrevStat(2, isDisplayed)]);
+            break;
+            
             case "p4" : info4.setText(information[getPrevStat(3, isDisplayed)]);
-                break;
+                        title4.setText(title[getPrevStat(3, isDisplayed)]);
+            break;
         }
     }
-    
+
     /**
      * Return the index of the information which has not been displayed
      * on the panel as the new information.
@@ -160,7 +172,7 @@ public class StatisticsController extends Controller
         isDisplayed[statIndex] = true;
         return statIndex;
     }
-    
+
     /**
      * Return the index of the information which has not been displayed
      * on the panel as the new information.
@@ -188,7 +200,7 @@ public class StatisticsController extends Controller
         isDisplayed[statIndex] = true;
         return statIndex;
     }
-    
+
     /**
      * Click "Back to Map" and move to map panel.
      */
