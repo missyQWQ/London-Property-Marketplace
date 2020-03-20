01586e3437a9bae98d4942f8ff50e198476d686a
Yichun Zhang <yichun.zhang@kcl.ac.uk>
2020-03-20 00:38:58 +0000

    Updated propertyList window.

---
+++ PropertiesListController.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class PropertiesListController here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PropertiesListController extends Controller
+{
+    
+}
